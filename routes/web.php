d39b32953a4c555a40ed392bf431ea78bc3ba9a3
zgf123 <277845885@qq.com>
2018-12-03 07:54:48 +0000

    notification_and_eventListener

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,13 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    // 开始
+    Route::group(['middleware' => 'email_verified'], function() {
+        
+    });
+    // 结束
+});
